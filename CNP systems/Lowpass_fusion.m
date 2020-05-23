1b798ae42124c3883b560cff14312cd963b8a2bb
MorvanLi <morvanli@foxmail.com>
2020-05-23 14:32:53 +0800

    first commit

--- CNP systems/Lowpass_fusion.m
+++ CNP systems/Lowpass_fusion.m
@@ -2,5 +2,5 @@ function [cp, map] =Lowpass_fusion(matrixA,matrixB)
 
 fire_matrixA = CNPS(abs(matrixA),110);
 fire_matrixB = CNPS(abs(matrixB),110);
-map=fire_matrixA >= fire_matrixB；
+map=fire_matrixA >= fire_matrixB;
 cp=map.*matrixA+~map.*matrixB;
